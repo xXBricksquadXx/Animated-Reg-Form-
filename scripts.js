ee8cf5ed0b9298175b8ebbb94004b3ab221d4ea1
Colby <colby88cardell@gmail.com>
2021-02-05 13:43:38 -0500

    :recycle: Added validation fields for 'regform'

--- scripts.js
+++ scripts.js
@@ -1,33 +1,62 @@
-const text = document.getElementById("text");
-window.addEventListener("scroll", () => {
-  const value = window.scrollY;
-  text.style.marginBottom = `${value * 2}px`;
-});
+const alreadyMsg = "Already have an account?";
+const needMsg = "Need to create an account?";
 
 const submitBtn = document.querySelector('button[type="submit"]');
 const toggleBtn = document.querySelector("#toggle-btn");
 
-const toggledInputs = Array.from(document.querySelectorAll("input")).slice(
-  0,
-  4
+const inputs = document.querySelectorAll("input");
+
+const toggledInputDivs = Array.from(document.querySelectorAll("div")).filter(
+  (div) => {
+    const input = div.querySelector("input");
+
+    return input && input.id !== "uname" && input.id !== "password";
+  }
 );
 
-toggleBtn.addEventListener("click", function () {
-  if (this.innerText === "Already have an account?") {
-    for (let i = 0; i < toggledInputs.length; i += 1) {
-      toggledInputs[i].classList.remove("is-visible");
-      toggledInputs[i].classList.add("is-hidden");
+function toggleFormInfo() {
+  toggledInputDivs.forEach((toggledInputDiv) => {
+    if (toggleBtn.innerText === alreadyMsg) {
+      toggledInputDiv.classList.replace("is-visible", "is-hidden") ||
+        toggledInputDiv.classList.add("is-hidden");
+    } else {
+      toggledInputDiv.classList.replace("is-hidden", "is-visible");
     }
-    this.innerText = "Need to create an account?";
-    submitBtn.innerText = "Login!";
-  } else {
-    const hiddenInputs = document.querySelectorAll("input.is-hidden");
-    for (let i = 0; i < hiddenInputs.length; i += 1) {
-      toggledInputs[i].classList.remove("is-hidden");
-      hiddenInputs[i].classList.add("is-visible");
-    }
+  });
 
-    this.innerText = "Already have an account?";
-    submitBtn.innerText = "Register!";
-  }
+  toggleBtn.innerText =
+    toggleBtn.innerText === alreadyMsg ? needMsg : alreadyMsg;
+
+  submitBtn.innerText = submitBtn.innerText.includes("Login")
+    ? "Register!"
+    : "Login!";
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("blur", function handleBlur() {
+    if (this.value.length) {
+      this.classList.replace("is-error", "is-valid") ||
+        this.classList.add("is-valid");
+
+      // Use optional chaining to prevent error if there is no 'nextElementSibling'
+      this.nextElementSibling?.classList.replace("is-visible", "is-hidden");
+    } else {
+      this.classList.replace("is-valid", "is-error") ||
+        this.classList.add("is-error");
+
+      this.nextElementSibling?.classList.replace("is-hidden", "is-visible");
+    }
+  });
+
+  input.addEventListener("focus", function handleFocus() {
+    this.classList.remove("is-error", "is-valid");
+  });
+});
+
+toggleBtn.addEventListener("click", toggleFormInfo);
+
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  console.log(event.target.elements);
 });
